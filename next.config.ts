01a90a7289821564d2398b050450126e79704011
Marlena Fryman <marlena.fryman@harrison.kyschools.us>
2025-09-08 18:43:22 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -32,6 +32,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'iibb.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
