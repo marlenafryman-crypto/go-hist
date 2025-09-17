e93657d5d140186e796c7790c2aae38d2826dcae
Marlena Fryman <marlena.fryman@harrison.kyschools.us>
2025-09-17 20:16:13 +0000

    https://nextjs.org/docs/messages/module-not-found

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -3,5 +3,4 @@ config();
 
 import '@/ai/flows/get-historical-suggestions.ts';
 import '@/ai/flows/find-matching-card.ts';
-import '@/ai/flows/verify-hist-set.ts';
 import '@/ai/flows/get-ai-player-action.ts';
